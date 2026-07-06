ae49b073122f7a56b5c6ad31cbe6eff577a58256
junghyun <neukdol30@gmail.com>
2026-07-06 10:17:50 +0900

    first

---
+++ src/main/java/com/siyan1234/itproject2nd/ItProject2ndApplication.java
@@ -0,0 +1,13 @@
+package com.siyan1234.itproject2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItProject2ndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItProject2ndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siyan1234/itproject2nd/ItProject2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siyan1234.itproject2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItProject2ndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
