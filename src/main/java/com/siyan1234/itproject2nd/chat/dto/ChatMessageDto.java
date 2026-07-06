6693805b4d1d67a94051ad02850ae8ab3576d925
tjoeunis205 <fkrdbs1278@gmail.com>
2026-07-06 11:59:59 +0900

    Dto 작성

--- src/main/java/com/siyan1234/itproject2nd/chat/dto/ChatMessageDto.java
+++ src/main/java/com/siyan1234/itproject2nd/chat/dto/ChatMessageDto.java
@@ -1,4 +1,19 @@
 package com.siyan1234.itproject2nd.chat.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
 public class ChatMessageDto {
+    private Integer messageNo;
+    private Integer roomNo;
+    private Integer senderNo;
+    private String messageContent;
+    private String readYn;
+    private LocalDateTime createdDate;
 }
--- src/main/java/com/siyan1234/itproject2nd/chat/dto/ChatRoomDto.java
+++ src/main/java/com/siyan1234/itproject2nd/chat/dto/ChatRoomDto.java
@@ -1,4 +1,21 @@
 package com.siyan1234.itproject2nd.chat.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
 public class ChatRoomDto {
+    private Integer roomNo;
+    private Integer userNo;
+    private Integer adminNo;
+    private String status;
+    private String lastMessage;
+    private LocalDateTime lastMessageDate;
+    private LocalDateTime createdDate;
+    private LocalDateTime closedDate;
 }
