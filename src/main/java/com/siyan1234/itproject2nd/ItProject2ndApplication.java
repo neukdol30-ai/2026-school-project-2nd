209bef9a5583f6945f90534ac86291012c9f877a
beom101 <cksrn7188@gmail.com>
2026-07-07 13:55:52 +0900

    main-develop

--- src/main/java/com/siyan1234/itproject2nd/ItProject2ndApplication.java
+++ src/main/java/com/siyan1234/itproject2nd/ItProject2ndApplication.java
@@ -1,8 +1,10 @@
 package com.siyan1234.itproject2nd;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan
 @SpringBootApplication
 public class ItProject2ndApplication {
 
---
+++ src/main/java/com/siyan1234/itproject2nd/member/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.siyan1234.itproject2nd.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    private Long no;
+    private String memberId;
+}
