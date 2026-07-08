6c7dd50513e6bc9642be42ea6deb8da914abe143
beom101 <cksrn7188@gmail.com>
2026-07-08 15:14:09 +0900

    관리자 페이지 접근 권한 구현

---
+++ src/main/java/com/siyan1234/itproject2nd/admin/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.siyan1234.itproject2nd.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping({"", "/"})
+    public String adminMain() {
+        return "admin/main";
+    }
+}
