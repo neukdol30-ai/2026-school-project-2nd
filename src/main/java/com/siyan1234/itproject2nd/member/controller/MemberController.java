ca31dc373748c7f1f4b2cffaeb7683205d21b5c0
Jaeheon <tyrua1@naver.com>
2026-07-09 15:17:20 +0900

    게시판 공지사항 댓글까지 TOAST UI

--- src/main/java/com/siyan1234/itproject2nd/member/controller/MemberController.java
+++ src/main/java/com/siyan1234/itproject2nd/member/controller/MemberController.java
@@ -71,4 +71,54 @@ public class MemberController {
 
         return "member/login";
     }
+
+    @PostMapping("/login")
+    public String loginProcess(LoginDto loginDto,
+                               HttpSession session,
+                               Model model) {
+
+        MemberDto loginMember = memberService.login(loginDto);
+
+        if (loginMember == null) {
+            model.addAttribute("error", "아이디 또는 비밀번호가 틀렸습니다.");
+            return "member/login";
+        }
+
+        session.setAttribute("loginMember", loginMember);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupProcess(MemberDto memberDto,
+                                RedirectAttributes redirectAttributes) {
+        try {
+            memberService.signup(memberDto);
+            return "redirect:/member/login";
+
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/member/signup";
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/id-check")
+    public Map<String, Boolean> idCheck(@RequestParam(required = false) String memberId) {
+
+        boolean isDuplicate = memberService.isDuplicateMemberId(memberId);
+
+        return Collections.singletonMap("isDuplicate", isDuplicate);
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
 }
