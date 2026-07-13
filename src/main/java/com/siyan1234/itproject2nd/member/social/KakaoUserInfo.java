e2d984b3b64753dc9f45776eb9552fb3c1632082
beom101 <cksrn7188@gmail.com>
2026-07-13 17:57:58 +0900

    소셜 로그인 사용자 정보 인터페이스 추가 및 생년월일 오류 메시지 한글화

---
+++ src/main/java/com/siyan1234/itproject2nd/member/social/KakaoUserInfo.java
@@ -0,0 +1,32 @@
+package com.siyan1234.itproject2nd.member.social;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class KakaoUserInfo implements SocialUserInfo {
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public String getNickname() {
+        return "";
+    }
+
+    @Override
+    public String getEmail() {
+        return "";
+    }
+
+    @Override
+    public String getProvider() {
+        return "";
+    }
+
+    @Override
+    public String getProviderId() {
+        return "";
+    }
+}
